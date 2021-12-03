3ab7bdf82e3dfb0cf13baac4d8d53cbe11b948bc
40225143 <msinclair06@qub.ac.uk>
2021-12-03 22:49:31 +0000

    added error handling

--- src/functions.inc.php
+++ src/functions.inc.php
@@ -2,5 +2,9 @@
 // get count of words - no error checking
 function wordcount($text)
 {
-    return str_word_count($text);
+    if($text != "")
+    {
+        return str_word_count($text);
+    }
+  
 }
--- src/index.php
+++ src/index.php
@@ -6,15 +6,37 @@ require('functions.inc.php');
 $output = array(
 	"error" => false,
 	"string" => "",
-	"answer" => 0
+	"answer" => 0,
+	"statusCode" => ""
 );
+
 
 $t = $_REQUEST['text'];
 
-$answer=wordcount($t);
+if($t ==null)
+{
+	header('HTTP/1.1 404 Not Found');
+	http_response_code(404);
+	$answer="N/A";
+	$error = true;
+	$output['string']="Please enter text";
+	$output['answer']=$answer;	
+	$output['error'] = $error;
+	$output['statusCode'] = 404;
+	echo json_encode($output);
+}
+else {
+	http_response_code(200);
+	$answer=wordcount($t);
+	$error = false;
+	$output['string']="Contains ".$answer." words";
+	$output['answer']=$answer;
+	$output['error'] = $error;
+	$output['statusCode'] = 200;
+	echo json_encode($output);
 
-$output['string']="Contains ".$answer." words";
-$output['answer']=$answer;
+}
 
-echo json_encode($output);
+
+
 exit();
